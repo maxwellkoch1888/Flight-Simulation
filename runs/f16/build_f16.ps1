d35e18753860077778ff3e49f6c1dce315bf70bc
maxwellkoch <maxtheskier@icloud.com>
2025-10-27 19:14:15 -0600

    Stuck on 7.8.1 during study hall.

--- runs/f16/build_f16.ps1
+++ runs/f16/build_f16.ps1
@@ -3,3 +3,4 @@ gfortran -fdefault-real-8 json/json.f90 json/jsonx.f90 modules/koch.f90 modules/
 # run with 
 # .\run_f16.exe ..\..\json\f16.json
 # use .. to go up a level in the directory
+# json_get(j_main,"key",val,found(logical))
