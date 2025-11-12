37d1a46b6ae2d1990402775c0deb43b2839082a5
maxwellkoch <maxtheskier@icloud.com>
2025-11-12 01:10:27 -0700

    Back to pc

--- graphics/display.py
+++ graphics/display.py
@@ -1,6 +1,8 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+import time 
+
 from quaternion import quat_mult, quat_norm, quat_to_euler, eul_to_quat
 
 # OPEN THE JSON FILE
@@ -90,68 +92,77 @@ class Camera:
         self.nvp = np.cross(P1 - self.P0, P2 - self.P0)
             
 class Lines:
-    def __init__(self, grid_number:int, grid_scale:float):
+    def __init__(self, grid_number:int, grid_scale:float, altitude:float, ax):
         # BUILD POINTS TO BE PLOTTED 
-        xf = []
-        yf = []
-        zf = []
-        self.max_distance = grid_number * grid_scale
-        i = -self.max_distance
-        while i <= self.max_distance:
-            j = -self.max_distance
-            while j <= self.max_distance:
-                xf.append(i)
-                yf.append(j)
-                zf.append(altitude)
-                j += grid_scale
-            i += grid_scale
+        nxlines = grid_number * 2 + 1 
+        self.nlines = 2*nxlines
+        self.points = np.zeros((4*nxlines,3))
+        self.lines = np.zeros((2*nxlines,2), dtype=int)
+        for i in range(nxlines):
+            self.points[2*i,   :] = [-grid_number*grid_scale, (i-grid_number)*grid_scale, -altitude]
+            self.points[2*i+1, :] = [ grid_number*grid_scale, (i-grid_number)*grid_scale, -altitude]
+            self.lines[i,0] = 2*i
+            self.lines[i,0] = 2*i+1
 
-        self.xf = np.array(xf)
-        self.yf = np.array(yf)
-        self.zf = np.array(zf)
+        for i in range(nxlines):
+            self.points[2*i+2*nxlines  , :] = [(i-grid_number)*grid_scale, -grid_number*grid_scale, -altitude]
+            self.points[2*i+2*nxlines+1, :] = [(i-grid_number)*grid_scale,  grid_number*grid_scale, -altitude]
+            self.lines[i+nxlines,0] = 2*i*nxlines 
+            self.lines[i+nxlines,1] = 2*i*nxlines+1
 
-    def intersecting_points(self, nvp, P0, quat, location):
-        # CALCULATE THE LINE FORM THE CAMERA TO EACH POINT OF INTEREST
-        lca = [] 
-        gamma = []
-        point_vp = []
-        xvp = []
-        yvp = []
-        points = np.size(self.xf)
-        i = 0
+        self.npoints = len(self.points)
+        self.nlines = len(self.lines)
 
-        while i < points:
-            point = np.array([[self.xf[i]], [self.yf[i]], [self.zf[i]]])
-            distance = point.flatten() - location
-            lca.append(distance.flatten())
-            # print('point:', point)
-            # print('distance:', distance)
-            # print('lca:', lca )
-            # print('P0:', P0)
-            # print('camera_location:', location)
+        # SET UP AXES FOR DRAWING
+        self.ax, = ax.plot([], [], marker='', ls='-', color=color)
 
-            # CALCULATE GAMMA
-            gam = np.matmul((P0-location), nvp) / np.matmul(distance, nvp)
-            gamma.append(gam.flatten())
+        # ALLOCATE SPACE FOR 3D POINTS IN EARTH FIXED COORDINATES
+        self.points3D = np.zeros((self.npoints,3))
+        # self.set_state([0.0, 0.0, 0.0,], [1.0, 0.0, 0.0, 0.0])
 
-            e0 = quat[0]
-            ex = quat[1]
-            ey = quat[2]
-            ez = quat[3]            
-            
-            rotation_matrix = np.array([[ex**2 + e0**2 - ey**2 - ez**2, 2*(ex*ey + ez*e0), 2*(ex*ez - ey*e0)],
-                                        [2*(ex*ey - ez*e0), ey**2 + e0**2 - ex**2 - ez**2, 2*(ey*ez + ex*e0)],
-                                        [2*(ex*ez + ey*e0), 2*(ey*ez - ex*e0), ez**2 + e0**2 - ex**2 - ey**2]])
-            
-            # CALCULATE xvp, yvp, zvp
-            point_vp.append(np.matmul(rotation_matrix, (gam*distance)))
-            i += 1
+        # ALLOCATE SPACE FOR 2D POINTS ON VIEWPLANE
+        self.points2D = np.zeros((self.npoints,2))
+        self.lca = np.zeros((self.npoints,3))
+        self.t = np.zeros(self.npoints)
+        self.pf = np.zeros((self.npoints,3))
 
-        self.lca = np.array(lca)
-        self.gamma = np.array(gamma)
-        self.point_vp = np.array(point_vp)
-        self.x_vp = self.point_vp[:,1]
-        self.y_vp = -self.point_vp[:,2]
+        self.lines2D = np.full((self.nlines*3,2), None, dtype=object)
+
+    def plot(self, camera):
+        pc = camera.location
+        lc0 = (camera.P0 - pc.flatten()).flatten()
+        temp = np.dot(lc0, camera.nvp)
+
+        self.lca[:] = self.points3D[:] - pc.flatten()
+
+        self.t[:] = temp/np.dot(self.lca,camera.nvp)
+
+        self.pf[:] = self.lca[:]*self.t[:, None]
+
+        e0, ex, ey, ez = camera.quat
+        a00 = ex*ex + e0*e0 - ey*ey - ez*ez 
+        a01 = 2*(ex*ey - ez*e0)
+        a02 = 2*(ex*ez - ey*e0)
+        a10 = 2*(ex*ey - ez*e0)
+        a11 = ey*ey + e0*e0 - ex*ex - ez*ez 
+        a12 = 2*(ey*ez - ex*e0)
+        a20 = 2*(ex*ez - ey*e0)
+        a21 = 2*(ey*ez - ex*e0)
+        a22 = ez*ez + e0*e0 - ey*ey - ex*ex
+
+        self.points2D[:,0] =  a01*self.pf[:,0] + a11*self.pf[:,1] + a21*self.pf[:,2]
+        self.points2D[:,1] = -a02*self.pf[:,0] - a12*self.pf[:,1] - a22*self.pf[:,2]
+        
+        for i in range(self.nlines):
+            i0 = self.lines[i,0]
+            i1 = self.lines[i,1]
+
+            # if(self.t[i0]>0 and self.t[i1]>0):
+            #     self.lines2D[3*i,  :] = self.points2D[i0,:]
+            #     self.lines2D[3*i+1,:] = self.points2D[i1,:]
+            # elif(self.t[i0]<0 and self.t[i1]<0):
+
+        self.ax.set_data(self.lines2D[:,0], self.lines2D[:,1])
 
 # BUILD THE CAMERA
 camera = Camera(
@@ -169,17 +180,31 @@ camera.set_state(
     quat=quat_init
     )
 
+# PLOT THE FIGURE
+fig = plt.figure(figsize=(camera.vp_aspect_ratio*5.0,5.0))
+ax = fig.add_subplot(111)
+plt.subplots_adjust(top=1.0, bottom=0.0, left=0.0, right=1.0)
+plt.axis('off')
+ax.axes.set_xlim(camera.vp_yv[0], camera.vp_yv[2])
+ax.axes.set_ylim(-camera.vp_zv[1], -camera.vp_zv[0])
+ax.axes.xaxis.set_ticklabels([])
+ax.axes.yaxis.set_ticklabels([])
+ax.set_xticks([])
+ax.set_yticks([])
+ax.axes.set_aspect('equal')
+fig.canvas.draw()
+# ground.plot()
+# plt.show()
+plt.show(block=False)
+
 # BUILD THE LINES
-lines = Lines(
+ground = Lines(
             grid_number=grid_number,
-            grid_scale=grid_scale
+            grid_scale=grid_scale,
+            altitude=altitude,
+            ax=ax
             )
-lines.intersecting_points(
-    nvp=camera.nvp, 
-    P0=camera.P0, 
-    quat=quat_init,
-    location=camera_location.flatten()
-    )
+
 
 # PRINT RESULTS
 print('------------------------------------ Results ------------------------------------')
@@ -206,32 +231,29 @@ print(f'P0:      {camera.P0}')
 print(f'n_vp:    {camera.nvp}')
 print(f'P0 - PC: {(camera.P0 - camera_location.flatten())}')
 print('lca: ')
-print(lines.lca)
-print('gamma: ')
-print(lines.gamma)
-np.set_printoptions(precision=12, suppress=True)
-print('x_vp: ')
-print(lines.x_vp)
-print('y_vp: ')
-print(lines.y_vp)
+# print(ground.lca)
+# print('gamma: ')
+# print(ground.gamma)
+# np.set_printoptions(precision=12, suppress=True)
+# print('x_vp: ')
+# print(ground.x_vp)
+# print('y_vp: ')
+# print(ground.y_vp)
 
-# PLOT THE FIGURE
-fig = plt.figure(figsize=(camera.vp_aspect_ratio*5.0,5.0))
-ax = fig.add_subplot(111)
-plt.subplots_adjust(top=1.0, bottom=0.0, left=0.0, right=1.0)
-plt.axis('off')
-ax.axes.set_xlim(camera.vp_yv[0], camera.vp_yv[2])
-ax.axes.set_ylim(-camera.vp_zv[1], -camera.vp_zv[0])
-ax.axes.xaxis.set_ticklabels([])
-ax.axes.yaxis.set_ticklabels([])
-ax.set_xticks([])
-ax.set_yticks([])
-ax.axes.set_aspect('equal')
-fig.canvas.draw()
-# plt.plot(lines.x_vp, lines.y_vp, 'o', color=color) 
-# plt.show()
-plt.show(block=False)
+# EXAMPLE SIMULATION
+location = camera.location 
+quat = camera.quat 
 
+while(location[0] < 0):
+    time_begin = time.time()
+    camera.set_state(location=location, quat=quat)
+    ground.plot(camera=camera)
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+    location[0] += 0.01
+    time_end = time.time()
+    fps = 1/(time_end - time_begin)
+    print('graphics rate [hz]:', fps)
 
 # # PLOT THE FIGURE
 # fig = plt.figure(figsize=(camera.vp_aspect_ratio*5.0,5.0))
