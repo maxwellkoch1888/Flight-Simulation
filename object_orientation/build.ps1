0f3dbe92bba12dd3c45325e653c262a31fe8759f
maxwellkochpc <maxwellkoch8@gmail.com>
2026-01-11 15:05:34 -0700

    Reorganized object orientation again.

--- object_orientation/build.ps1
+++ object_orientation/build.ps1
@@ -1,10 +1,10 @@
 $root = $PSScriptRoot
 
 gfortran -ffree-line-length-512 -fdefault-real-8 `
-"$root\source\json.f90" `
-"$root\source\jsonx.f90" `
-"$root\source\linalg_mod.f90" `
-"$root\source\micro_time.f90" `
+"$root\source\helper\json.f90" `
+"$root\source\helper\jsonx.f90" `
+"$root\source\helper\linalg_mod.f90" `
+"$root\source\helper\micro_time.f90" `
 "$root\source\koch.f90" `
 "$root\source\vehicle.f90" `
 "$root\source\sim.f90" `
