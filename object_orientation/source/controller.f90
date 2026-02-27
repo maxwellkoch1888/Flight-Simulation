305651011bb069495d73700958059df913ad26bf
maxwellkochpc <maxwellkoch8@gmail.com>
2026-02-26 21:53:50 -0700

    Untested controller update, cascade controller.

--- object_orientation/source/controller.f90
+++ object_orientation/source/controller.f90
@@ -16,7 +16,7 @@ module controller_m
   end type pid_t
 
   type controller_t 
-    type (pid_t), allocatable :: pid_controllers(:) 
+    type (pid_t) :: p_da, q_de, r_dr, bank_p, gamma_q, V_tau
     integer :; num_pid 
     type(connnection) : pilot_conn 
     logical :: running = .false. 
@@ -24,7 +24,7 @@ module controller_m
 
   contains 
     
-    subroutine contorller_init(t, j_main) 
+    subroutine controller_init(t, j_main) 
       implicit none 
       type(controller_t), intent(inout) :: t 
       type(json_value), pointer :: j_main, j_connections, j_pid, j_temp 
@@ -41,13 +41,24 @@ module controller_m
 
       write(*,*) 'Initializing PID Controllers...'
       call jsonx_get(j_main, 'PID', j_pid)
-      t%num_pid = json_value_count(j_pid) 
-      allocate(t%pid_controller(i), j_temp)
 
-      do i = 1, t%num_pid
-        call json_value_get(j_pid, i, j_temp) 
-        call pid_init(t%pid_contollers(i), j_temp) 
-      end do 
+      call json_value_get(j_pid, 'p->aileron', j_temp) 
+      call pid_init(t%p_da, j_temp) 
+
+      call json_value_get(j_pid, 'q->elevator', j_temp) 
+      call pid_init(t%q_de, j_temp) 
+      
+      call json_value_get(j_pid, 'r->rudder', j_temp) 
+      call pid_init(t%r_dr, j_temp)
+
+      call json_value_get(j_pid, 'bank->p', j_temp) 
+      call pid_init(t%bank_p, j_temp)
+
+      call json_value_get(j_pid, 'gamma->q', j_temp) 
+      call pid_init(t%gamma_q, j_temp)
+
+      call json_value_get(j_pid, 'V->throttle', j_temp) 
+      call pid_init(t%V_tau, j_temp)
 
       write(*,*) 'Controller Initialization Complete.'
     end subroutine controller_init 
@@ -76,6 +87,7 @@ module controller_m
         t%display_units = 1.0 
       end if 
       call jsonx_get(j_pid, 'limits', t%limit, 0.0, 2) 
+      call jsonx_get(j_pid, 'dyp_schedule', t%dyp_schedule, .false.)
       t%limit(:) = t%limit(:)/t%display_units 
 
     end subroutine pid_init 
@@ -86,17 +98,45 @@ module controller_m
       type(controller_t), intent(inout) :: t 
       real, intent(in) :: states(21), time 
       real :: ans(4) 
-      real :: omega_command(3), omega-actual(3) 
+      real :: u, v, w, p, q, r
+      real :: eul(3), sp, st, cp, ct, 
+      real :: Vmag, gamma, g 
       real :: Z, Temp, P, rho, a, mu, dyp 
+      real :: pilot_command(3), bank_sp, gamma_sp, V_sp, p_sp, q_sp, r_sp
 
-      ans = 
+      u = states(1)
+      v = states(2)
+      w = states(3)
+      p = states(4)
+      q = states(5)
+      r = states(6)
 
-      omega_actual = states(4:6) 
+      eul = quat_to_euler(states(10:13))
+      sp  = sin(eul(1))
+      st  = sin(eul(2))
+      cp  = cos(eul(1))
+      ct  = cos(eul(2))
+
+      Vmag = sqrt(u**2 + v**2 + w**2) 
+
+      gamma = asin((u*st - (v*sp + w*cp) * ct) / Vmag) 
       call std_atm_English(-states(9), Z, Tmep, P, rho, a, mu) 
-      dyp = 0.5 * rho * (states(1) **2 + states(2)**2 _ states(3)**2)
+      dyp = 0.5 * rho * Vmag**2
+      g = gravity_English(-states(9))
 
-      omega_command = t%pilot_conn%recv([time], time) 
-      ans(1) = pid_get_command(t%pid_controllers(1), omega_command(1), omega_actual(1), time, dyp) 
+      pilot_command = t%pilot_conn%recv([time], time)
+      bank_sp  = pilot_command(1) * pi / 180.0 
+      gamma_sp = pilot_command(2) * pi /180.0 
+      V_sp     = pilot_command(3) 
+
+      p_sp = pid_get_command(t%bank_p, bank_sp, eul(1), time, dyp)
+      q_sp = pid_get_command(t%gamma_q, gamma_sp, gamma, time, dyp)
+      r_sp = (g*sp*ct + p*w)/u ! neglect gravity relief
+
+      ans(1) = pid_get_command(t%p_da,  p_sp, p,    time, dyp)
+      ans(2) = pid_get_command(t%q_de,  q_sp, q,    time, dyp)
+      ans(3) = pid_get_command(t%r_dr,  r_sp, r,    time, dyp)
+      ans(4) = pid_get_command(t%V_tau, V_sp, Vmag, time, dyp)
 
     end function controller_update
 
@@ -112,6 +152,7 @@ module controller_m
         t%error_int   = 0.0 
         t%error_deriv = 0.0 
         ans           = t%KP * t%prev_error 
+        if(t%dyp_schedule) ans = ans/dyp 
         t%prev_ans    = ans 
         return 
       end if 
@@ -134,6 +175,7 @@ module controller_m
 
         ! PID equation 
         ans = (t%KP*t%error + t%KI*t%error_int + t%KD*t%error_deriv) / dyp 
+        if(t%dyp_schedule) ans = ans/dyp 
 
         t%prev_error = t%error 
         t%prev_time  = time 
